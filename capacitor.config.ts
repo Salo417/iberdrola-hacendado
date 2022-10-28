de5e6e4dce1fa9ef955e6177458b0d4bf49d93c6
Salo25 <schooldayssal@gmail.com>
2022-10-28 10:04:56 +0200

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'iberdrola-hacendado',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
