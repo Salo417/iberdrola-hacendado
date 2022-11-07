f85dc44a148ae3acaebed2a5d92a653a7bacd4e5
Salo25 <schooldayssal@gmail.com>
2022-11-07 08:30:59 +0100

    v0.4 - Home page slider prototype done

--- src/app/pages/home/home.page.ts
+++ src/app/pages/home/home.page.ts
@@ -1,9 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, ViewEncapsulation } from '@angular/core';
+import { SwiperComponent } from 'swiper/angular';
+import SwiperCore, { Navigation } from 'swiper';
+
+
+SwiperCore.use([Navigation]);
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
+  encapsulation: ViewEncapsulation.None,
 })
 export class HomePage {
 
